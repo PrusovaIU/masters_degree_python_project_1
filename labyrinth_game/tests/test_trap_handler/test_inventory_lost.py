401be8381b47f58e3b0f52ed5bf542dec0216278
Iuliia <prusova.work@gmail.com>
2025-10-16 22:18:26 +0300

    добавлены тесты для _inventory_lost

---
+++ labyrinth_game/tests/test_trap_handler/test_inventory_lost.py
@@ -0,0 +1,53 @@
+from collections.abc import Generator
+
+import pytest
+from labyrinth_game import trap_handler
+from unittest.mock import patch, Mock
+from labyrinth_game.constants.item import Items
+
+
+@pytest.fixture
+def mock_pseudo_random() -> Generator[Mock, None, None]:
+    """
+    Патч функции pseudo_random.
+
+    :return: мок.
+    """
+    with patch.object(trap_handler, "pseudo_random") as mock:
+        yield mock
+
+
+def test_inventory_lost_from_empty_inventory(
+        mock_game_state: Mock,
+        mock_pseudo_random: Mock
+) -> None:
+    """
+    Тест проверяет, что при пустом инвентаре ничего не происходит.
+
+    :param mock_game_state: мок состояния игры.
+    :param mock_pseudo_random: мок функции pseudo_random.
+    :return: None.
+    """
+    mock_game_state.player.inventory = []
+    trap_handler._inventory_lost(mock_game_state)
+    mock_pseudo_random.assert_not_called()
+
+
+def test_inventory_lost_from_not_empty_inventory(
+        mock_game_state: Mock,
+        mock_pseudo_random: Mock
+) -> None:
+    """
+    Тест проверяет, что при не пустом инвентаре происходит потеря предмета.
+
+    :param mock_game_state: мок состояния игры.
+    :param mock_pseudo_random: мок функции pseudo_random.
+    :return: None.
+    """
+    mock_game_state.player.inventory = [Items.gold_coin, Items.rusty_key]
+    mock_pseudo_random.return_value = 0
+
+    trap_handler._inventory_lost(mock_game_state)
+
+    mock_pseudo_random.assert_called_once()
+    assert mock_game_state.player.inventory == [Items.rusty_key]
